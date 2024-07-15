fedcd20b9c88d1f299c221ad0e87089ca582c772
juanAriel <arielvia2013@gmail.com>
2024-07-15 18:30:28 -0400

    configuracion de routa en api.

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/students', function(){
+    return 'This is student page';
+});
